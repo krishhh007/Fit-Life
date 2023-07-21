3f382209c7358f70693119e042fdd9434581c904
Krishnaraj S <krishnarajshetty699@gmail.com>
2023-07-21 23:43:05 +0530

    WELCOME Page Added

---
+++ lib/fancy_loader.dart
@@ -0,0 +1,25 @@
+
+import 'package:fitlife/fancy_loader_state.dart';
+import 'package:flutter/material.dart';
+
+enum AnimationType {
+  gradient,
+  pulsing,
+  rotating,
+}
+
+class FancyLoader extends StatefulWidget {
+  final AnimationType animationType;
+  final double size;
+  final Duration duration;
+
+  const FancyLoader({
+    Key? key,
+    this.animationType = AnimationType.gradient,
+    this.size = 40.0,
+    this.duration = const Duration(milliseconds: 800), required Color startColor, required Color endColor,
+  }) : super(key: key);
+
+  @override
+  FancyLoaderState createState() => FancyLoaderState();
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:fitlife/fancy_loader.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -11,15 +12,169 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'FitLife',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.orange,
       ),
-      home: const GetStarted(title: 'BMI CALC'),
+      home: const WelcomePage(title: 'FitLife',),
     );
   }
 }
+
+
+class WelcomePage extends StatefulWidget {
+  const WelcomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _WelcomePageState createState() => _WelcomePageState();
+}
+
+class _WelcomePageState extends State<WelcomePage>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _animationController;
+  late Animation<double> _fadeInAnimation;
+  late Animation<double> _buttonScaleAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 800),
+    );
+
+    // Create the fade-in animation for the "Welcome to FitLife!" text
+    _fadeInAnimation = Tween<double>(begin: 0.0, end: 1.0).animate(
+      CurvedAnimation(
+        parent: _animationController,
+        curve: Curves.easeInOut,
+      ),
+    );
+
+    // Create the button scale animation for the "Get Started" button
+    _buttonScaleAnimation = Tween<double>(begin: 1.0, end: 1.1).animate(
+      CurvedAnimation(
+        parent: _animationController,
+        curve: Curves.easeInOut,
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    _animationController.forward();
+
+    return Scaffold(
+      body: Center(
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              // Animated "Welcome to FitLife!" text with gradient effect
+              FadeTransition(
+                opacity: _fadeInAnimation,
+                child: ShaderMask(
+                  shaderCallback: (bounds) {
+                    return const LinearGradient(
+                      colors: [Colors.orange, Colors.green],
+                      stops: [0.0, 1.0],
+                    ).createShader(bounds);
+                  },
+                  child: const Text(
+                    'Welcome to FitLife!',
+                    style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 50),
+              // Use SizedBox with fixed dimensions to properly display FancyLoader
+              const SizedBox(
+                width: 100,
+                height: 500,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    FancyLoader(
+                      animationType: AnimationType.gradient,
+                      size: 50.0,
+                      duration: Duration(milliseconds: 1000),
+                      startColor: Colors.blue,
+                      endColor: Colors.green,
+                    ),
+                    SizedBox(height: 20),
+                    FancyLoader(
+                      animationType: AnimationType.pulsing,
+                      size: 50.0,
+                      duration: Duration(milliseconds: 800),
+                      startColor: Colors.blue,
+                      endColor: Colors.green,
+                    ),
+                    SizedBox(height: 20),
+                    FancyLoader(
+                      animationType: AnimationType.rotating,
+                      size: 50.0,
+                      duration: Duration(milliseconds: 1200),
+                      startColor: Colors.blue,
+                      endColor: Colors.green,
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 20),
+              // Animated Circular and Red "Get Started" Button with scale effect
+              ScaleTransition(
+                scale: _buttonScaleAnimation,
+                child: Material(
+                  borderRadius: BorderRadius.circular(50),
+                  color: Colors.red,
+                  child: InkWell(
+                    onTap: () => _goToGetStarted(context),
+                    borderRadius: BorderRadius.circular(50),
+                    child: Container(
+                      width: 150,
+                      height: 50,
+                      alignment: Alignment.center,
+                      child: const Text(
+                        'Get Started',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  // Method to navigate to the GetStarted page
+  void _goToGetStarted(BuildContext context) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => GetStarted(title: widget.title),
+      ),
+    );
+  }
+}
+
+        
+
+
 
 class GetStarted extends StatelessWidget {
   const GetStarted({Key? key, required String title});
@@ -180,20 +335,20 @@ class _HomePageState extends State<HomePage>
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.blueGrey,
         hintColor: Colors
             .green, // Set the accent color to green for the ElevatedButton
-        textTheme: TextTheme(
+        textTheme: const TextTheme(
           headline6: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
           bodyText1: TextStyle(fontSize: 20),
         ),
       ),
       home: Scaffold(
-        backgroundColor: const Color.fromARGB(255, 218, 215, 215),
+        backgroundColor:  Colors.white,
         appBar: AppBar(
           title: const Text('BMI Calculator'),
           centerTitle: true,
-          backgroundColor: const Color.fromARGB(255, 2, 94, 169),
+          backgroundColor: Colors.amberAccent,
         ),
         body: GestureDetector(
           onTap: () {
@@ -232,36 +387,35 @@ class _HomePageState extends State<HomePage>
                             controller: _heightController,
                             decoration: InputDecoration(
                               labelText: 'Height (meters)',
-                              prefixIcon: Icon(Icons.height),
+                              prefixIcon: const Icon(Icons.height),
                               border: OutlineInputBorder(
                                 // Add Material Design border
                                 borderRadius: BorderRadius.circular(8.0),
                               ),
-                              fillColor: Colors.lime,
+                              fillColor:  const Color.fromARGB(255, 207, 210, 178),
                               // Set the background color of the field
                               filled:
                                   true, // Set to true to fill the field with the specified color
                             ),
                             keyboardType:
-                                TextInputType.numberWithOptions(decimal: true),
+                                const TextInputType.numberWithOptions(decimal: true),
                           ),
                           const SizedBox(height: 16),
                           TextFormField(
                             controller: _weightController,
                             decoration: InputDecoration(
                               labelText: 'Weight (kilograms)',
-                              prefixIcon: Icon(Icons.line_weight),
+                              prefixIcon: const Icon(Icons.line_weight),
                               border: OutlineInputBorder(
                                 // Add Material Design border
                                 borderRadius: BorderRadius.circular(8.0),
                               ),
-                              fillColor: Colors
-                                  .lime, // Set the background color of the field
+                              fillColor: const Color.fromARGB(255, 207, 210, 178), // Set the background color of the field
                               filled:
                                   true, // Set to true to fill the field with the specified color
                             ),
                             keyboardType:
-                                TextInputType.numberWithOptions(decimal: true),
+                                const TextInputType.numberWithOptions(decimal: true),
                           ),
                         ],
                       ),
